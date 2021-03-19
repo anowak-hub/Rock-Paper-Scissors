1130991c7dc6882bc593614913f6a30ed49186bd
anowak-hub <anowak4320@stu.d214.org>
2021-03-18 20:02:02 -0400

    Finished MVP

--- Rock, Paper, Scissors/ViewController.swift
+++ Rock, Paper, Scissors/ViewController.swift
@@ -36,18 +36,76 @@ class ViewController: UIViewController {
     @IBAction func whenGameTapGestureTapped(_ sender: UITapGestureRecognizer) {
         let selectedPoint = sender.location(in: choiceStackView)
         
+        var userSelection = 0
+        
         for choice in choicesImageViews {
             if choice.frame.contains(selectedPoint) {
                 let currentImage = choice.image
                 playerImageView.image = currentImage
-            }
-        }
-        
-        for random in choicesImageViews {
-            if random.frame.contains(selectedPoint) {
+                
+                if currentImage == choicesImageViews[0].image {
+                    userSelection = 0
+                }
+                if currentImage == choicesImageViews[1].image {
+                    userSelection = 1
+                }
+                if currentImage == choicesImageViews[2].image {
+                    userSelection = 2
+                }
+            
+            
             
             }
         }
+        
+       
+        
+        
+        let randomNumber = Int.random(in: 0...2)
+        if randomNumber == 0 {
+            computerImageView.image = choicesImageViews[0].image
+        }
+        if randomNumber == 1 {
+            computerImageView.image = choicesImageViews[1].image
+        }
+        if randomNumber == 2 {
+            computerImageView.image = choicesImageViews[2].image
+        }
+   
+        
+        
+        if playerImageView.image == computerImageView.image {
+            winnerLabel.text = "Tie"
+        }         
+        
+        
+        if (randomNumber == 0) && (userSelection == 1) {
+            winnerLabel.text = "You Lose"
+        }
+        if (randomNumber == 0) && (userSelection == 2) {
+            winnerLabel.text = "You Win!"
+        }
+        
+        
+        if (randomNumber == 1) && (userSelection == 0) {
+            winnerLabel.text = "You Win!"
+        }
+        if (randomNumber == 1) && (userSelection == 2) {
+            winnerLabel.text = "You Lose"
+        }
+        
+        
+        if (randomNumber == 2) && (userSelection == 0) {
+            winnerLabel.text = "You Lose"
+        }
+        if (randomNumber == 2) && (userSelection == 1) {
+            winnerLabel.text = "You Win!"
+        }
+    
+    
+    
+    
+    
     
     
     }
